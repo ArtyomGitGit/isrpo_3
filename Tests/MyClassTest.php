0a68bcc9e482ae6bb18ec8867db3ab3f525df49b
Artyom <mrtomcheket@yandex.ru>
2022-03-29 23:11:59 +0300

    add tests

---
+++ Tests/MyClassTest.php
@@ -0,0 +1,32 @@
+<?php
+namespace ISRPO\Tests;
+
+use ISRPO\MyClass;
+use PHPUnit\Framework\TestCase;
+
+class MyClassTest extends TestCase
+{
+    protected $myClass;
+    protected function setUp(): void
+    {
+        $this->myClass = new MyClass;
+    }
+
+    protected function tearDown(): void
+    {
+        
+    }
+    public function testHelloWorld()
+    {
+        $this->assertEquals('Hello world!', $this->myClass->helloWorld());
+    }
+
+    public function testPower()
+    {
+        $this->assertEquals(9, $this->myClass->power(3, 2));
+    }
+
+}
+
+$obj = new MyClass();
+echo $obj->helloWorld();
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+namespace ISRPO;
+
+require_once __DIR__ . join(DIRECTORY_SEPARATOR, ['', 'vendor', 'autoload.php']);
+
+echo $obj->helloWorld();
